d86376869a6e6bc9bc0ea18a6f2571a3c07be5df
Bart de Ruijter <bederuijter@gmail.com>
2015-04-05 19:37:36 +0200

    Init default state

--- Webshop/Webshop/Models/AccountModels.cs
+++ Webshop/Webshop/Models/AccountModels.cs
@@ -11,7 +11,7 @@ namespace Webshop.Models
     public class UsersContext : DbContext
     {
         public UsersContext()
-            : base("DefaultConnection")
+            : base("WebshopDbEntities")
         {
         }
 
